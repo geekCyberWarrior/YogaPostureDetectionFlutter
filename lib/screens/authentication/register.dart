28f167f805c10ae7cbf257d82761623fa4152ebc
geekCyberWarrior <sanjithjadhav365@gmail.com>
2021-06-14 22:40:08 +0530

    Form Validation and Toast Added

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yoga_posture_detection/net/flutterfire.dart';
 import 'package:yoga_posture_detection/screens/authentication/login.dart';
 import 'package:yoga_posture_detection/screens/homeView.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Regsiter extends StatefulWidget {
   const Regsiter({Key? key}) : super(key: key);
@@ -14,10 +15,12 @@ class _RegsiterState extends State<Regsiter> {
   TextEditingController _nameField = TextEditingController();
   TextEditingController _emailField = TextEditingController();
   TextEditingController _passwordField = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -51,108 +54,161 @@ class _RegsiterState extends State<Regsiter> {
                     SizedBox(
                       height: 20.0,
                     ),
-                    TextFormField(
-                      controller: _nameField,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          borderSide: BorderSide(color: Color(0xffC4C4C4)),
-                        ),
-                        fillColor: Color(0xffC4C4C4),
-                        filled: true,
-                        labelText: "Name",
-                        labelStyle: TextStyle(
-                          color: Color(0xff313030),
-                          fontSize: 18.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    TextFormField(
-                      controller: _emailField,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          borderSide: BorderSide(color: Color(0xffC4C4C4)),
-                        ),
-                        fillColor: Color(0xffC4C4C4),
-                        filled: true,
-                        hintText: "something@email.com",
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          color: Color(0xff313030),
-                          fontSize: 18.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    TextFormField(
-                      controller: _passwordField,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          borderSide: BorderSide(color: Color(0xffC4C4C4)),
-                        ),
-                        fillColor: Color(0xffC4C4C4),
-                        filled: true,
-                        hintText: "password",
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          color: Color(0xff313030),
-                          fontSize: 18.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        bool shouldNavigate = await register(_nameField.text,
-                            _emailField.text, _passwordField.text);
-                        if (shouldNavigate) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomeView(),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Your Name';
+                              }
+                              return null;
+                            },
+                            controller: _nameField,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                                borderSide:
+                                    BorderSide(color: Color(0xffC4C4C4)),
+                              ),
+                              fillColor: Color(0xffC4C4C4),
+                              filled: true,
+                              labelText: "Name",
+                              labelStyle: TextStyle(
+                                color: Color(0xff313030),
+                                fontSize: 18.0,
+                              ),
                             ),
-                          );
-                        }
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Text(
-                          "REGISTER",
-                          style: TextStyle(
-                            fontSize: 18.0,
                           ),
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0xff282B24),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Email';
+                              }
+                              String pattern =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                              RegExp regex = new RegExp(pattern);
+                              return (!regex.hasMatch(value))
+                                  ? 'Please Enter Correct Email'
+                                  : null;
+                            },
+                            controller: _emailField,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                                borderSide:
+                                    BorderSide(color: Color(0xffC4C4C4)),
+                              ),
+                              fillColor: Color(0xffC4C4C4),
+                              filled: true,
+                              hintText: "something@email.com",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                color: Color(0xff313030),
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Password';
+                              }
+                              return null;
+                            },
+                            controller: _passwordField,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                                borderSide:
+                                    BorderSide(color: Color(0xffC4C4C4)),
+                              ),
+                              fillColor: Color(0xffC4C4C4),
+                              filled: true,
+                              hintText: "password",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                color: Color(0xff313030),
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                bool shouldNavigate = await register(
+                                    _nameField.text,
+                                    _emailField.text,
+                                    _passwordField.text);
+                                if (shouldNavigate) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HomeView(),
+                                    ),
+                                  );
+                                  Fluttertoast.showToast(
+                                      msg: "Register successful!!",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                      backgroundColor:
+                                          Color.fromRGBO(29, 28, 28, 0.5),
+                                      timeInSecForIosWeb: 1,
+                                      fontSize: 16.0);
+                                }
+                              }
+                            },
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Text(
+                                "REGISTER",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xff282B24),
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
